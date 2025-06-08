e79af5a772894a33ac241a788e2e5b4daab14cc3
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-08 12:18:54 +0530

    Created the sign-up function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,32 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
   const { fullName, email, password } = req.body
   try {
     // hash password
+    if(password.length < 6) {
+      return res.status(400).json( { message: "Password must be at least 6 characters "});
+    }
+
+    const user = await User.findOne({email})
+
+    if (user) return res.status(400).json({ message: "Email already exists"});
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User ({
+      fullName,
+      email,
+      password: hashedPassword
+    })
+
+    if (newUser) {
+      //generate jwt taken here
+    }else {
+      res.status(400).json({ message: "Invalid user data"});
+    }
   } catch (error) {
     
   }
