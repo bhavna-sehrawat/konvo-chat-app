57c3c286966721a33a79f4152cf681540b8ac0c2
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-15 17:45:04 +0530

    Updated

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useState } from 'react';
+import { useAuthStore } from '../store/useAuthStore';
 
 const SignUpPage = () => {
-  return (
-    <div>
-      Signup
-    </div>
-  )
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName:"",
+    email:"",
+    password:"",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  };
+  return <div className="min-h-screen grid lg:grid-cols-2"></div>;
 }
 
 export default SignUpPage
