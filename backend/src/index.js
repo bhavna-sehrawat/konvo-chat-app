4dd1508ef99fbf22ec0d09908e9f2640599460a0
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-19 11:34:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path"
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -18,19 +18,17 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors (
- { origin: "http://localhost:5173",
-  credentials: true}
-))
-
-app.use(express.json({ limit: '50mb' }));
-// Good practice to increase the urlencoded limit, just in case.
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
@@ -42,4 +40,3 @@ server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
   connectDB();
 });
-
