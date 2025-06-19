76c98e8d76df1726db94ad0cde46bec05583b743
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-19 11:19:23 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,10 +5,10 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server( server, {
+const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"]
-  }
+    origin: ["http://localhost:5173"],
+  },
 });
 
 export function getReceiverSocketId(userId) {
@@ -16,15 +16,15 @@ export function getReceiverSocketId(userId) {
 }
 
 // used to store online users
-const userSocketMap = {}; // { userId: socketId}
+const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId
-  if(userId) userSocketMap[userId] = socket.id
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-  //io.emit is used to send events to all the connected clients
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
