af7927d8daafa3c8e9e9896149c7cac302dc1199
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-19 12:23:38 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get(/(.*)/, (req, res) => {
+  app.get(/(.*)/, (req, res) => { // wildcard * not supported anymore, use /(.*)/
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
