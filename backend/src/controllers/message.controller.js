34256f7a227ae95c13da0d15ef877bc4a8b500df
Bhavna <sehrawatbhavna001@gmail.com>
2025-06-19 11:44:27 +0530

    Updated to remove circular dependency during deplyment

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 
 import cloudinary from "../lib/cloudinary.js";
-import { getReceiverSocketId, io } from "../lib/socket.js";
+import { getReceiverSocketId, getIO } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -60,7 +60,7 @@ export const sendMessage = async (req, res) => {
 
     const receiverSocketId = getReceiverSocketId(receiverId);
     if(receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
+      getIO.to(receiverSocketId).emit("newMessage", newMessage);
     }
 
     res.status(200).json(newMessage);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,37 +1,42 @@
 import { Server } from "socket.io";
-import http from "http";
-import express from "express";
 
-const app = express();
-const server = http.createServer(app);
+let io;
+const userSocketMap = {}; // { userId: socketId }
 
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-  },
-});
-
-export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
-}
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-
-  const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
-
-  // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+export const initSocketIO = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173", // This will use our Render environment variable
+      credentials: true,
+    },
   });
-});
 
-export { io, app, server };
+  io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if (userId) userSocketMap[userId] = socket.id;
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected", socket.id);
+      if (userId) {
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+      }
+    });
+  });
+};
+
+export const getReceiverSocketId = (userId) => {
+  return userSocketMap[userId];
+};
+
+// This new function lets our controller get the io instance when needed
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.IO not initialized!");
+  }
+  return io;
+};
